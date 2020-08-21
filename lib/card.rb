c4f49586cffe137da8c5e35d9eac1d1df934b514
Robert Heath <robert.b.heathii@gmail.com>
2020-08-21 07:09:38 -0500

    Test class

---
+++ lib/card.rb
@@ -0,0 +1,13 @@
+class Card
+  # attr_reader :suit, :rank, :value
+  #
+  # def initalize(suit, rank, value)
+  # @suit = suit
+  # @rank = rank
+  # @value = value
+  # end
+
+  # def setup
+  #   card = Card.new(:diamond, 'Queen', 12)
+  # end
+end
