1c08af6586ef5f515e72fcf95e30e8f95d8d37d2
Robert Heath <robert.b.heathii@gmail.com>
2020-08-22 22:00:05 -0500

    Code and test rank_of_card_at

--- lib/deck.rb
+++ lib/deck.rb
@@ -5,9 +5,46 @@ require 'pry'
 class Deck
   attr_reader :cards
 
-  def initialize
-    @cards = []
+  def initialize(cards = [])
+    @cards = cards
   end
+
+  def rank_of_card_at(number)
+    card = cards[number]
+    card.rank
+  end
+
+# high_ranked_cards = []
+#
+#   def high_ranking_cards
+#
+#     card.each do |high_ranking|
+#       high_ranking = []
+#       if card.rank = 'Jack'
+#         high_ranking << card
+#       elsif card.rank = 'Queen'
+#         high_ranking << card
+#       elsif card.rank = 'King'
+#         high_ranking << card
+#       elsif cark.rank = 'Ace'
+#         high_ranking << card
+#       else
+#         next
+#       end
+#       high_ranked_cards << high_ranking
+#     end
+#
+#   def percent_high_ranking
+#     high_ranked_cards.length / cards.length
+#   end
+#
+#   def remove_card
+#     cards = cards.shift
+#   end
+#
+#   def add_card(card)
+#     cards = cards << card
+#   end
 
   # def cards
   #   @cards
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -7,13 +7,21 @@ require 'pry'
 class DeckTest < Minitest::Test
 
   def test_it_exists
-    deck = Deck.new
+    deck = Deck.new([])
     assert_instance_of Deck, deck
   end
 
   def test_deck_has_a_card_attribute
-    deck = Deck.new
+    deck = Deck.new([])
 
     assert_equal [], deck.cards
   end
+
+  def test_rank_of_card_at
+    card1 = Card.new(:diamond, 'Queen', 12)
+    deck1 = Deck.new([card1])
+    assert_equal 12, deck1.rank_of_card_at(0)
+  end
+
+
 end
